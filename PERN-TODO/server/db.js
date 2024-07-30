92340b4f05fdfe2a0717243732b8b1248aaf2aa9
Mohite <suraj.mohite@metro-gsc.in>
2024-07-30 12:02:34 +0530

    Eleventh commit -> Setup of Environment Variables for server side deployment

--- PERN-TODO/server/db.js
+++ PERN-TODO/server/db.js
@@ -10,8 +10,18 @@ const devConfig = {
 };
 
 const proConfig = {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
+
+// const devConfig = {
+//     connectionString: "postgresql://perntodoapp_user:i8Cp2u7MzeRN6qiNeKLmmtFIhzNqJjvv@dpg-cqjrkgogph6c739d56tg-a.oregon-postgres.render.com/perntodoapp",
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// }
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = pool;
